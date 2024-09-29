0e34e48513fb157d03fe5fd952ffe48401743078
Vivek <yadav100adu@gmail.com>
2024-09-29 18:18:24 +0530

    Setup

---
+++ src/app/pages/allowed-voter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const allowedVoter = () => {
+  return (
+    <div>allowed-voter</div>
+  )
+}
+
+export default allowedVoter
---
+++ src/app/pages/candidates-registration.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const candidatesRegistration = () => {
+  return (
+    <div>candidatesRegistration</div>
+  )
+}
+
+export default candidatesRegistration
